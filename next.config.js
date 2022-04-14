1bbe106c988803faa0e40d613b955b6a3b2cf21e
choiminwoo98 <cmw0918@naver.com>
2022-04-14 23:20:55 +0900

    commit2
    complete

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const API_KEY = process.env.API_KEY;
 
-console.log(API_KEY);
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
@@ -18,6 +17,10 @@ const nextConfig = {
       {
         source: "/api/movies",
         destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
       },
     ];
   },
